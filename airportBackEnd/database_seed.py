f2e8711ef468886d2892650f24f8f56f2b5de922
Rodolfo Gonzalez <rudynoot123@gmail.com>
2025-05-02 11:21:46 -0500

    Initialized vite,tailwind,docker,fastapi

---
+++ airportBackEnd/database_seed.py
@@ -0,0 +1,9 @@
+import pymysql
+
+connection = pymysql.connect(
+    host = "localhost",
+    user ="root",
+    password="",
+    database=""
+)
+airlines [] = {"American Airlines","Delta","United Airlines","Southwest Airlines","JetBlue"} 
