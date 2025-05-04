0554a77083bf9a1f497eb8e8a1804543c581797f
Rodolfo Gonzalez <rudynoot123@gmail.com>
2025-05-04 00:14:30 -0500

    Fix vite tailwind config

--- airportFrontEnd/src/App.jsx
+++ airportFrontEnd/src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from 'react'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <p>
-        Hello
-      </p>
+    <div className="w-full h-full absolute bg-white">
     </div>
   )
 }
